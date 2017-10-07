a2635af9d0e4e3cac3f77b2adec593ade39f224c
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2017-10-07 11:10:30 -0500

    Tipping
    Fixed tipping limit to -300

--- LoaderBot.java
+++ LoaderBot.java
@@ -1,15 +1,12 @@
 /*
 Copyright 2017
-
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify, merge, publish, distribute,
 sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial
 portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
 NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -78,9 +75,11 @@ public class NewLoaderBot extends LinearOpMode {
                         //Boom controlling code
                         if(boom_limit.getState()){
                                 //Boom is not touching bottom limit
-                                if(boom_lift.getCurrentPosition() < 130){
+                                if(boom_lift.getCurrentPosition() > -300){
                                         //Boom is not far back enough to tip
                                         boom_lift.setPower(this.gamepad1.right_stick_y);
+                                }else{
+                                        boom_lift.setPower(0);
                                 }
                         } else {
                                 //Boom is touching bottom limit
